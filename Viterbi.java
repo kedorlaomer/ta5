65e386ad1fd976aa7e576ee3142b297d75bc5350
Alexander Adler <alexander.adler0@googlemail.com>
2013-01-25 10:49:15 +0100

    started implementing Viterbi algorithm

---
+++ Viterbi.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Viterbi
+{
+    private int k;
+    private HMMLearner[] learners;
+    private double[] α;
+    private String[] allTags;
+
+    /*
+     * reads an up to order k HMM model from directory's corpus
+     * files, weighting different models with non-zero
+     * coefficients α
+     */
+
+    public Viterbi(File directory, int k, double[] α) throws IOException
+    {
+        this.k = k;
+        this.α = α;
+
+        learner = HMMLearner[k];
+
+        for (int i = 0; i < k; i++)
+            learners[i] = new HMMLearner(directory, i+1);
+    }
+
+    /*
+     * input: "this/xxx is/xxx an/xxx untagged/xxx sentence/xxx"
+     * output: "this/det is/bez an/at untagged/jj sentence/nn"
+     */
+
+    public TaggedToken[] tagSentence(TaggedToken[] sentence)
+    {
+        
+    }
+
+    /*
+     * Returns the probability for token getting tag with this
+     * history. 
+     */ 
+
+    private double probability(String[] history, String token, String tag)
+    {
+        double rv = 0;
+        double denom = 0;
+
+        for (int i = 0; i < k; i++)
+        {
+            String[] h = new String[i];
+            System.arraycopy(history, k-i-1, h, 0, i);
+
+            double p = learners[i].probability(h, token, tag);
+            if (!Double.isNaN(p))
+            {
+                rv += p;
+                denom += α[i];
+            }
+        }
+
+        // TODO: Back-off-Model einfügen! Soll sein eigenes α
+        // bekommen!
+
+        return rv/denom;
+    }
+}
